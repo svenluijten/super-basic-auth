32b65ba9b5668045c49a913b1a9689bc57f4060b
Sven Luijten <svenluijten1995@gmail.com>
2019-09-15 13:34:13 +0200

    Remove unnecessary boilerplate

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,19 +6,5 @@ use GrahamCampbell\TestBench\AbstractPackageTestCase;
 
 abstract class TestCase extends AbstractPackageTestCase
 {
-    /**
-     * Set up the testing suite.
-     */
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
-
-    /**
-     * Tear down the testing suite.
-     */
-    public function tearDown(): void
-    {
-        parent::tearDown();
-    }
+    //
 }
